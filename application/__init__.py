4aac034cba3cf63937666e0c3986ff4eaf1b26da
adrianozanetti1978 <adrianogzanetti@gmail.com>
2024-09-12 19:44:31 -0300

    Atualização

--- application/__init__.py
+++ application/__init__.py
@@ -8,22 +8,22 @@ import os
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_class=None):
-    app = Flask(__name__, template_folder='templates')
-    
-    if config_class:
-        app.config.from_object(config_class)
-    else:
-        app.config.from_object('application.config.Config')  # Padrão
+def create_app():
+    app = Flask(__name__, template_folder='templates')  # Especifica o diretório de templates e estático
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '238779Adri')  # Substitua por uma chave secreta real
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:238779Adri@localhost:5432/usuario_db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     with app.app_context():
+        # Registrar Blueprints
         from application.routes import main
         app.register_blueprint(main)
 
-        from application.models import Account, NewAccount
+        # Criação de tabelas e inicialização do banco de dados
+        from application.models import Account, NewAccount  # Agora inclui o NewAccount
         db.create_all()
 
     return app
